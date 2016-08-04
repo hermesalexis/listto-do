9fdb14656d4a4d4988df2d7f45f201d7b765a252
hermesagm <hermesagm@gmail.com>
2016-08-04 13:53:18 -0500

    Metodo tasks_by_state para segmentar tareas por estado

--- listo_do.rb
+++ listo_do.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require 'make_todo'
 
+def tasks_by_state(state)
+  completed_task = []	
+  Tarea.all.each do |task|
+    completed_task << task if task.fetch('done') == state  
+  end
+  completed_task
+end
+
+
 get '/' do
  @tasks = Tarea.all #Recovery all tasks in a array of hashes
  erb:index
@@ -11,3 +20,4 @@ post '/done' do
  @tasks = Tarea.all #Recovery all tasks whit the new state
  erb:index
 end
+
