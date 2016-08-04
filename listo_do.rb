56c19b8937158c7a00072863bdece71041786d4e
hermesagm <hermesagm@gmail.com>
2016-08-04 15:46:29 -0500

    Lista Tareas Ordenadas por estado

--- listo_do.rb
+++ listo_do.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'make_todo'
 
+	
 def tasks_by_state(state)
   completed_task = []	
   Tarea.all.each do |task|
@@ -9,15 +10,23 @@ def tasks_by_state(state)
   completed_task
 end
 
+def join_tasks
+ tasks_completed = tasks_by_state(true)
+ tasks_incompleted = tasks_by_state(false)	
+ tasks_incompleted.concat tasks_completed
+end
+
 
 get '/' do
- @tasks = Tarea.all #Recovery all tasks in a array of hashes
+ @tasks = join_tasks
  erb:index
 end
 
 post '/done' do 
- Tarea.update(params[:id]) #update state of task (id)
- @tasks = Tarea.all #Recovery all tasks whit the new state
+ Tarea.update(params[:id]) #Actualiza el estado de las tareas(id)
+ @tasks = join_tasks
  erb:index
 end
+
+
 
