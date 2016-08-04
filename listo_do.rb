6a35f9655234e3ed9b64834727c182fa9daf4246
hermesagm <hermesagm@gmail.com>
2016-08-04 13:17:24 -0500

    radio buttons para elegir tareas completas e incompletas

--- listo_do.rb
+++ listo_do.rb
@@ -2,7 +2,12 @@ require 'sinatra'
 require 'make_todo'
 
 get '/' do
- @tasks = Tarea.all
+ @tasks = Tarea.all #Recovery all tasks in a array of hashes
  erb:index
+end
 
+post '/done' do 
+ Tarea.update(params[:id]) #update state of task (id)
+ @tasks = Tarea.all #Recovery all tasks whit the new state
+ erb:index
 end
